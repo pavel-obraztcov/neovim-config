6948c9669c287e5eefdc81357bcb42e91b0e8ee5
Pavel Obraztcov <pavel.obraztcov@sky.de>
2023-06-16 11:56:12 +0200

    Made some additional remaps shamelessly copied from theprimeagen

--- lua/pavel/init.lua
+++ lua/pavel/init.lua
@@ -1,1 +1,2 @@
 require("pavel.remap")
+require("pavel.set")
--- lua/pavel/remap.lua
+++ lua/pavel/remap.lua
@@ -1,2 +1,41 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- this is for moving text around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- append the next line to the end of the current line not moving the cursor
+vim.keymap.set("n", "J", "mzJ`z")
+-- keep the cursor at the middle of the screen when moving
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste over a selection without replacing the value in the register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+--use <leader>y to copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- delete to the void register
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+
+-- Quick Fix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace the word you're on
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Make the current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- source file more easily
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
