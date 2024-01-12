811c25442aa2817d57d0fd408e7ef84fe291c5e7
Pavel Obraztcov <Pavel.Obraztcov@sky.de>
2024-01-12 10:14:20 +0000

    Added some remaps for a terminal in Chrome

--- lua/pavel/remap.lua
+++ lua/pavel/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", "<C-w>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- this is for moving text around
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
