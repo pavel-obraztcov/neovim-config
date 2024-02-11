a7712452f62964e9e7630e416ddc42cda8463896
Pavel Obraztcov <pavel.obraztcov@sky.de>
2024-02-11 14:07:48 +0100

    I dont't really like trailing commas. Never remember which languages are OK with them

--- after/plugin/yarepl.lua
+++ after/plugin/yarepl.lua
@@ -19,7 +19,7 @@ yarepl.setup {
     scroll_to_bottom_after_sending = true,
     os = {
         windows = {
-            send_delayed_cr_after_sending = true,
+            send_delayed_cr_after_sending = true
         }
     }
 }
